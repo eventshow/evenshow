20478af11283bccba32b3e02e11f5268ea9fdd49
Miguel <migfuecalclever@gmail.com>
2020-03-24 12:49:45 +0100

    [fix] details

--- events/forms.py
+++ events/forms.py
@@ -7,7 +7,7 @@ from .models import Category, Event, Rating
 
 CHOICES_YES_NO = ((0, "No"), (1, "Sí"))
 
-CHOICES_SCORE = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
+CHOICES_SCORE = (('--'," " ),(1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
 
 User = get_user_model()
 
