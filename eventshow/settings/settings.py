2ce7cb9a17780710b6ed229f8e44a29cf07a5626
rojas1995 <jrojasgrando@gmail.com>
2020-03-26 22:27:51 +0100

    [fix] Añado la configuración del correo en el settings

--- eventshow/settings/settings.py
+++ eventshow/settings/settings.py
@@ -154,3 +154,12 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'contacteventshow@gmail.com'
+EMAIL_HOST_PASSWORD = 'isppevent20*'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
