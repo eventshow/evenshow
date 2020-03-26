03dc15c442ab509988ab498d5f796892487b7525
rojas1995 <jrojasgrando@gmail.com>
2020-03-26 22:37:04 +0100

    [doc] Añado las urls del reset para que sepa cuales son

--- events/urls.py
+++ events/urls.py
@@ -45,5 +45,11 @@ urlpatterns = [
     path('vista/gracias', TemplateView.as_view(template_name='event/thanks.html'),
          name='gracias'),
 
+
     url('^', include('django.contrib.auth.urls')),
 ]
+''' URLs of reset passaword: 
+        /password_reset
+        /password_reset/done
+        reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/
+        reset/done'''
