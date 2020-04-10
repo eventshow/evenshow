4cfb1f1a8906635322590bc2b4c70bc9409a24e1
angbongon <angbongon@alum.us.es>
2020-04-10 19:58:46 +0200

    [refactor] Eliminado selector innecesario

--- events/selectors.py
+++ events/selectors.py
@@ -23,12 +23,12 @@ class EnrollmentSelector:
 class EventSelector:
     def hosted(self, host: User) -> QuerySet:
         hosted_events = models.Event.objects.filter(
-            created_by=host).order_by('title')
+            created_by=host)
         return hosted_events
 
     def enrolled(self, attendee: User, status='ACCEPTED') -> QuerySet:
         enrolled_events = models.Event.objects.filter(
-            event_enrollments__created_by=attendee).order_by('title')
+            event_enrollments__created_by=attendee)
         return enrolled_events
 
     def not_enrolled(self, attendee: User) -> QuerySet:
@@ -38,11 +38,6 @@ class EventSelector:
 
     def rated_by_user(self, user: User, on='HOST') -> QuerySet:
         return models.Event.objects.filter(ratings__created_by=user, ratings__on=on)
-
-    def event_enrolled_accepted(self, attendee: User, status='ACCEPTED') -> QuerySet:
-        event_enrolled_accepted = models.Event.objects.filter(
-            event_enrollments__created_by=attendee, event_enrollments__status=status).order_by('title')
-        return event_enrolled_accepted
 
 
 class RatingSelector:
