5224be9a07f81988f579eb529b2a101c2daae338
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2020-04-11 12:20:28 +0200

    [feat] No hat penalizacion si no hay asistentes

--- events/views.py
+++ events/views.py
@@ -264,6 +264,7 @@ class EventDeleteView(generic.DeleteView):
             attendees = selectors.UserSelector().event_attendees(event_pk).count()
             amount_host = services.PaymentService().fee(round(event.price*100))
             context['penalty'] = (amount_host*attendees)
+            context['attendees'] = attendees
 
         return context
 
