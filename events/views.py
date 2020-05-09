f0a54fdf81ff7930c6a25154a15323217bba7565
angbongon <angbongon@alum.us.es>
2020-05-09 18:31:29 +0200

    [fix] Linea no comentada en views daba error

--- events/views.py
+++ events/views.py
@@ -598,56 +598,56 @@ class EnrollmentUpdateView(generic.View):
                 pk=enrollment_pk).created_by
 
             # services.EmailService().send_email(
-                subject, body, [recipient.email])
+            #   subject, body, [recipient.email])
 
             return redirect('list_enrollments', event.pk)
         else:
             return redirect('/')
 
     def updatable(self, host):
-        enrollment_pk=self.kwargs.get('pk')
-        event_has_started=models.Enrollment.objects.get(
-            pk = enrollment_pk).event.has_started
+        enrollment_pk = self.kwargs.get('pk')
+        event_has_started = models.Enrollment.objects.get(
+            pk=enrollment_pk).event.has_started
         return services.EnrollmentService().host_can_update(host,
                                                             enrollment_pk) and services.EnrollmentService().is_pending(
             enrollment_pk) and not event_has_started
 
 
-@method_decorator(login_required, name = 'dispatch')
+@method_decorator(login_required, name='dispatch')
 class PasswordUpdateView(generic.UpdateView):
-    template_name='profile/update_password.html'
-    model=User
-    form_class=forms.PasswordUpdateForm
-    success_url=reverse_lazy('detail_profile')
+    template_name = 'profile/update_password.html'
+    model = User
+    form_class = forms.PasswordUpdateForm
+    success_url = reverse_lazy('detail_profile')
 
     def get_context_data(self, **kwargs):
-        context=super(PasswordUpdateView, self).get_context_data(**kwargs)
-        context['form']=self.form_class(user = self.object)
+        context = super(PasswordUpdateView, self).get_context_data(**kwargs)
+        context['form'] = self.form_class(user=self.object)
         return context
 
     def get_object(self):
         return self.request.user
 
 
-@method_decorator(login_required, name = 'dispatch')
+@method_decorator(login_required, name='dispatch')
 class RateHostView(generic.CreateView):
-    template_name='rating/rating.html'
-    model=models.Rating
-    form_class=forms.RatingForm
-    success_url='/events/enrolled'
+    template_name = 'rating/rating.html'
+    model = models.Rating
+    form_class = forms.RatingForm
+    success_url = '/events/enrolled'
 
     def get(self, request, *args, **kwargs):
-        created_by=request.user
-        event_exist=services.EventService().exist_event(self.kwargs.get('event_pk'))
+        created_by = request.user
+        event_exist = services.EventService().exist_event(self.kwargs.get('event_pk'))
         if (not event_exist):
             return redirect('home')
         else:
-            event=models.Event.objects.get(pk = self.kwargs.get('event_pk'))
-            exist_already_rating=selectors.RatingSelector().exists_this_rating_for_this_user_and_event(created_by,
+            event = models.Event.objects.get(pk=self.kwargs.get('event_pk'))
+            exist_already_rating = selectors.RatingSelector().exists_this_rating_for_this_user_and_event(created_by,
                                                                                                          event,
                                                                                                          event.created_by)
 
-            is_enrolled_for_this_event=services.EnrollmentService().user_is_enrolled_and_accepted(event.id,
+            is_enrolled_for_this_event = services.EnrollmentService().user_is_enrolled_and_accepted(event.id,
                                                                                                     created_by)
             host = event.created_by
             auto_rating = self.request.user.id == host.id
